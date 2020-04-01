a0968b215ae73656d22fdd439587f6e199a10483
ONSEN <62979382+onsen-yamagata@users.noreply.github.com>
2020-04-02 00:27:57 +0900

    Update formatConfirmedCases.ts

--- utils/formatConfirmedCases.ts
+++ utils/formatConfirmedCases.ts
@@ -3,7 +3,7 @@ type DataType = {
   value: number
   children: [
     {
-      attr: '陽性患者数'
+      attr: '陽性者数'
       value: number
       children: [
         {
@@ -51,7 +51,7 @@ type ConfirmedCasesType = {
 export default (data: DataType) => {
   const formattedData: ConfirmedCasesType = {
     検査実施人数: data.value,
-    陽性物数: data.children[0].value,
+    陽性者数: data.children[0].value,
     入院中: data.children[0].children[0].value,
     軽症中等症: data.children[0].children[0].children[0].value,
     重症: data.children[0].children[0].children[1].value,
