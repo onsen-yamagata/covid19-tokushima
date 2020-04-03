5fc41de89ff2549f3d40ae245d5cb30e15ba554b
ONSEN <62979382+onsen-yamagata@users.noreply.github.com>
2020-04-04 01:50:07 +0900

    Update formatGraph.ts

--- utils/formatGraph.ts
+++ utils/formatGraph.ts
@@ -19,9 +19,9 @@ export default (data: DataType[]) => {
   const today = new Date().toLocaleString({ timeZone: 'Asia/Tokyo' })
   let patSum = 0
   data
-    .filter(d => new Date(d['日付']).toLocaleString({ timeZone: 'Asia/Tokyo' }) < today)
+    .filter(d => new Date(d['日付']) < today)
     .forEach(d => {
-      const date = new Date(d['日付']).toLocaleString({ timeZone: 'Asia/Tokyo' })
+      const date = new Date(d['日付'])
       const subTotal = d['小計']
       if (!isNaN(subTotal)) {
         patSum += subTotal
