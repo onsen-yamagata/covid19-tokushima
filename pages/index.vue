476a913e976b4b637b889e2126b9f99f5ac7cc1b
ONSEN <62979382+onsen-yamagata@users.noreply.github.com>
2020-04-06 19:43:25 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,83 @@
+<template>
+  <div class="MainPage">
+    <page-header
+      :icon="headerItem.icon"
+      :title="headerItem.title"
+      :date="headerItem.date"
+    />
+    <whats-new class="mb-4" :items="newsItems" />
+    <v-row class="DataBlock">
+      <confirmed-cases-details-card />
+      <confirmed-cases-number-card />
+      <confirmed-cases-attributes-card />
+      <tested-number-card />
+      <telephone-advisory-reports-number-card />
+      <consultation-desk-reports-number-card />
+    </v-row>
+  </div>
+</template>
+
+<SCRIPT type="text/javascript">
+	function link(){
+		location.href='https://stopcovid19-yamagata.netlify.com/';
+	}
+	link();
+</SCRIPT>
+
 <script lang="ts">
-import Vue from 'Vue'
+import Vue from 'vue'
+import { MetaInfo } from 'vue-meta'
+import PageHeader from '@/components/PageHeader.vue'
+import WhatsNew from '@/components/WhatsNew.vue'
+import Data from '@/data/data.json'
+import News from '@/data/news.json'
+import ConfirmedCasesDetailsCard from '@/components/cards/ConfirmedCasesDetailsCard.vue'
+import ConfirmedCasesAttributesCard from '@/components/cards/ConfirmedCasesAttributesCard.vue'
+import ConfirmedCasesNumberCard from '@/components/cards/ConfirmedCasesNumberCard.vue'
+import TestedNumberCard from '@/components/cards/TestedNumberCard.vue'
+import TelephoneAdvisoryReportsNumberCard from '@/components/cards/TelephoneAdvisoryReportsNumberCard.vue'
+
 export default Vue.extend({
-  fetch({ redirect }) {
-    redirect(301, 'https://stopcovid19-yamagata.netlify.com/')
+  components: {
+    PageHeader,
+    WhatsNew,
+	ConfirmedCasesDetailsCard,
+    ConfirmedCasesNumberCard,
+    ConfirmedCasesAttributesCard,
+    TestedNumberCard
+  },
+  data() {
+    const data = {
+      Data,
+      headerItem: {
+        icon: 'mdi-chart-timeline-variant',
+        title: this.$t('山形県内の最新感染動向'),
+        date: Data.lastUpdate
+      },
+      newsItems: News.newsItems
+    }
+    return data
+  },
+  head(): MetaInfo {
+    return {
+      title: this.$t('山形県内の最新感染動向') as string
+    }
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.MainPage {
+  .DataBlock {
+    margin: 20px -8px;
+    .DataCard {
+      @include largerThan($medium) {
+        padding: 10px;
+      }
+      @include lessThan($small) {
+        padding: 4px 8px;
+      }
+    }
+  }
+}
+</style>
